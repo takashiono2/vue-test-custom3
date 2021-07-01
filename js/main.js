27867db4161dfd22cdbfad21fa7fc69efbbb7245
takashiono2 <takashiono710@gmail.com>
2021-07-01 17:24:23 +0900

    日付取得カスタム

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
-'use strict';
-{
-  
-}
+var app = new Vue({
+  el: '#app',
+  data: {
+    now: '00:00:00'
+  },
+  methods: {
+    time: function(){
+      var date = new Date();
+      let month = date.getMonth() + 1;
+      let dayofweek = date.getDay();
+      const dayname = ['日','月','火','水','木','金','土'];
+      this.now = date.getFullYear()+"年"+month +"月"+date.getDate()+"日"+dayname[dayofweek]+"曜日"+date.getHours()+ ":"+ date.getMinutes()+ ":" +date.getSeconds();
+    }
+  }
+});
